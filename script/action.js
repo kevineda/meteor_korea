02646862ba0ad7bfb37fd5d46298218a096d92dc
kevineda <kt203665@gmail.com>
2024-07-12 10:52:15 +0900

    0712_version1

---
+++ script/action.js
@@ -0,0 +1,17 @@
+$('.icon_music').click(function(){
+    $(this).toggleClass('on');
+/*     $('audio').fadeToggle(); */
+
+    var audio = $('audio#music')[0];
+
+   if (audio.paused)    {
+    audio.play();
+   }
+   else{
+    audio/paused();
+   }
+    
+   $('.wrap').toggleClass('on')
+   $('.text_box').toggleClass('on')
+})
+
